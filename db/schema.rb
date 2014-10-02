409c95d9d27dce9b48dba8224adb592d789ea386
Seth Charles <swcharl@gmail.com>
2014-10-01 19:42:34 -0500

    Configure local postgres database.
    Configure local postgres database.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140930002618) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "exercises", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
@@ -44,8 +47,8 @@ ActiveRecord::Schema.define(version: 20140930002618) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "workouts", force: true do |t|
     t.datetime "started_at"
